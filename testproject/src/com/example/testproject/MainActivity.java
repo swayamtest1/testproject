e063ca65cca8f25702c8dd71d16bf2b745aabbfa
swayamtest2 <swayam.test2@gmail.com>
2013-09-04 17:52:19 +0530

    Update MainActivity.java

--- testproject/src/com/example/testproject/MainActivity.java
+++ testproject/src/com/example/testproject/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("Demo application");
 		setContentView(R.layout.activity_main);
 	}
 
