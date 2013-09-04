deea1bace5c57f69e77a537c80359771217cc18a
swayam <swayam.test1@gmail.com>
2013-09-04 10:34:07 +0530

    second ...

--- testproject/src/com/example/testproject/MainActivity.java
+++ testproject/src/com/example/testproject/MainActivity.java
@@ -12,17 +12,20 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Log.v("Java file", "File changes...");
-		
-		Log.v("Java file", "File changes...");
 		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		
+		Log.v("Java file", "File changes...");
+		
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+		
+		
+		
 	}
 
 }
