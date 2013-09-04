86ac5e0074bfd2497caee11d6189ff41df9c557c
swayamtest2 <swayam.test2@gmail.com>
2013-09-04 17:53:21 +0530

    New Messages...

--- testproject/src/com/example/testproject/erer.java
+++ testproject/src/com/example/testproject/erer.java
@@ -6,6 +6,7 @@ public class erer {
 	public static void main(String[] arg)
 	{
 		System.out.println("Demo application");
-		
+		System.out.println("Demo application");
+		System.out.println("Demo application");
 	}
 }
