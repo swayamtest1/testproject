6ff932cb3b9ccfc8040d600f4e191ac84d531e63
swayam <swayam.test1@gmail.com>
2013-09-04 13:07:12 +0530

    last update...

--- testproject/src/com/example/testproject/MainActivity.java
+++ testproject/src/com/example/testproject/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.v("print static message ","custom message...");
 		int i=0;
 		do
 		{
