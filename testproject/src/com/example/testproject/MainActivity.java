8122c0ad56028bccfb3ce643434bb7f2095e896b
swayam <swayam.test1@gmail.com>
2013-09-04 10:26:07 +0530

    Small message changes...

--- testproject/src/com/example/testproject/MainActivity.java
+++ testproject/src/com/example/testproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testproject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Log.v("Java file", "File changes...");
+		
+		Log.v("Java file", "File changes...");
+		
 	}
 
 	@Override
